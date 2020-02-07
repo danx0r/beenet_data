e83255c531b7890ac3688af90fe6c01b757a2476
danx0r <dan.miller@eye0.com>
2020-02-07 14:49:50 -0800

    manifestly mercurial

--- beenet_training_data/__init__.py
+++ beenet_training_data/__init__.py
@@ -1,1 +1,5 @@
-"All hail the BDFL"
+#All hail the BDFL
+
+def datapath():
+    i = __file__.rfind("/")
+    return __file__[:i+1]
--- setup.py
+++ setup.py
@@ -3,5 +3,6 @@ setup(
     name="beenet_training_data",
     version="0.0.1",
     packages=find_packages(),
-        python_requires='>=3',
+    python_requires='>=3',
+    include_package_data=True
 )
